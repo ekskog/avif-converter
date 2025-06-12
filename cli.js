46c14653643dc1da1392e2e55a9663471d03e394
Tayo Carvalhal <root@hbvu.su>
2025-06-12 20:56:11 +0200

    Rename heic-processor.js to server.js for better naming convention

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const HeicProcessor = require('./heic-processor');
+const HeicProcessor = require('./server');
 
 // CLI Usage function
 function showUsage() {
